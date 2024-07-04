195a1f980bd75bced871fee126040e692de85343
barrera77 <30675869+barrera77@users.noreply.github.com>
2024-07-04 00:52:10 -0600

    Impelemented Bonus Functionality

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import "@/styles/globals.css";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import { JobsProvider } from "../context/JobsContext";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <JobsProvider>
+      <Component {...pageProps} />
+    </JobsProvider>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,68 +1,16 @@
-import { useState, useEffect } from "react";
-
 import AvailableJobList from "@/components/AvailableJobList";
 import NavBar from "@/components/NavBar";
 import LoadingSpinner from "@/components/LoadingSpinner";
-
 import Container from "@mui/material/Container";
-
-import { getJobs } from "@/utils/api/jobs";
-
-const JOBS_ENDPOINT = "/api/jobs";
-const SAVED_JOBS_ENDPOINT = "/api/saved-jobs";
-
-//const savedJobs = [];
+import { useJobs } from "@/context/JobsContext";
 
 export default function Home() {
-  const [loading, setLoading] = useState(false);
-  const [jobs, setJobs] = useState([]);
-  const [savedJobs, setSavedJobs] = useState([]);
-
-  //fetch jobs on mount
-  useEffect(() => {
-    setLoading(true);
-    getJobs(JOBS_ENDPOINT)
-      .then((data) => {
-        console.log("Fetched jobs:", data);
-        setJobs(data);
-      })
-      .catch((error) => {
-        console.error("Error fetching jobs:", error);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, []);
-
-  //fetch saved-jobs on mount
-  useEffect(() => {
-    setLoading(true);
-    getJobs(SAVED_JOBS_ENDPOINT)
-      .then((data) => {
-        console.log("Fetched jobs:", data);
-        setSavedJobs(data);
-      })
-      .catch((error) => {
-        console.error("Error fetching jobs:", error);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, []);
-
+  const { loading, jobs } = useJobs();
   return (
     <main>
       <NavBar />
       <Container>
-        {loading ? (
-          <LoadingSpinner />
-        ) : (
-          <AvailableJobList
-            jobs={jobs}
-            savedJobs={savedJobs}
-            setSavedJobs={setSavedJobs}
-          />
-        )}
+        {loading ? <LoadingSpinner /> : <AvailableJobList jobs={jobs} />}
       </Container>
     </main>
   );
